d3e3d127ced6d0927e6657ca9e7e2307387c894e
Ербол Сыздық <ysyzdyk@alfabank.kz>
2019-07-11 04:53:24 +0600

    fix book open degree

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 console.log('hello world');
 console.log($('figure'));
-$('figure.book').click(function () {
+$('.book-wrap').click(function () {
   console.log(this);
   $(this).addClass('open')
 });
